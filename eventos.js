0760db82cbad7ac3539b3dfbccc39d1ad4eed25f
Alidoandres <141370844+Alidoandres@users.noreply.github.com>
2023-08-09 22:04:06 -0300

    create event.js
    eventlistenner al hacer click en el div con id "midiv" alert "hola soy el div"

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const div = document.getElementById("midiv");
+
+    div.addEventListener("click", function (){
+        alert ("hola soy el div")
+    })
+})
+
